ff03e92e1837829f24504dcd5f1021be4416f83b
DS4506 <davidsmith4506@gmail.com>
2023-12-12 21:44:26 -0600

    vauriables

---
+++ Scripts/practice.js
@@ -0,0 +1,35 @@
+console.log("practice")
+
+// student's info
+let studentname="David"
+let mygrade=4.5;
+let isStudent= true;
+const URL="Https://sdgku.edu"
+
+//dispaly values
+console.log(" my name is " + studentname);
+console.log("102 grade: " + mygrade);
+console.log(" Active: " +isStudent);
+
+let num1=10;
+let num2=5;
+
+console.log("multiplication: " + num1*num2);
+console.log("Addition: " +(num1+num2));
+console.log("Subtraction: "+ (num1-num2));
+console.log("Div: "+ (num1/num2));
+
+let assignmentName = "Class 1 - Assingment 1";
+let description= "Assignments are due the day Session 3 is held by 11:59 p.m. (in the time zone in which each student resides). Assignments represent 20% of the overall course grade.";
+let pointsAs1 = 6.66;
+let pointsAs2 = 6.66;
+let pointsAs3 = 6.66;
+
+let total = pointsAs1 + pointsAs2 + pointsAs3;
+let fileUploads= true;
+let studentAnnotation = false;
+
+let dueDate="12/20/2023";
+
+document.write(`<h1> ${assignmentName} </h1>`);
+document.write(`<p> ${description} </p>`);
